6d7633cc0cd57b2236ad9f6ab16c1e7fc5c0d467
Darshpreet2000 <47180477+Darshpreet2000@users.noreply.github.com>
2020-02-01 20:28:58 +0530

    Update Constants.java

--- app/src/main/java/com/example/myapplication/Util/Constants.java
+++ app/src/main/java/com/example/myapplication/Util/Constants.java
@@ -1,13 +1,8 @@
 package com.example.myapplication.Util;
 
 public class Constants {
-
-    public static final String REGEX_EMAIL = "^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$";
-
     public static String preferences = "preferences";
 
-    public static String NO_CONNECTION = "Please connect to internet";
-    public  static String ROLES="";
 
     // TEST API
 
@@ -16,36 +11,6 @@ public class Constants {
     // public static String AUTH_CLIENT_PASSWORD = "123";
 
 
-    // PRODUCTION
-
-    public static String AUTH_PASSWORD = "easyRclientUI";
-
-    public static String AUTH_CLIENT_PASSWORD = "easyRclientUI";
-
-
-
-
-    public static final String ACCESS_TOKEN= "access_token";
-
-    public static final String REFRESH_TOKEN= "refresh_token";
-
-    public static final String EXPIRE_IN= "expires_in";
-
-    public static final String CHANNEL_ID= "channel_id";
-
-    public static final String CHANNEL_NAME= "channel_name";
-
-    public static final String FACILITY_ID= "facility_id";
-
-    public static final String FACILITY_NAME= "facility_name";
-
-    public static final String CENTER_CODE= "center_code";
-
-    public static final String EDITION_NAME= "edition_name";
-
-    public static final String VALIDATE_INVENTORY= "validateInventory";
-
-    public static final String SUBSCRIPTION_STATUS = "subscription_status";
 
 
     public class Settings{
