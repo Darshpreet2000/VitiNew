8da9270780f83ac68dc692c98d78d001aff2f085
Darshpreet2000 <47180477+Darshpreet2000@users.noreply.github.com>
2020-02-01 18:00:40 +0530

    added register

---
+++ app/src/main/java/com/example/myapplication/Connections/UserController.java
@@ -0,0 +1,98 @@
+package com.example.myapplication.Connections;
+
+import android.content.Context;
+
+import com.example.myapplication.Webrequest.ResponseListener;
+
+import org.json.JSONObject;
+
+import java.util.Map;
+
+public class UserController {
+
+    private final Context mContext;
+    private UserModel mUserModel;
+
+
+    public UserController(final Context pContext) {
+        mContext = pContext;
+        mUserModel = new UserModel(pContext);
+    }
+
+
+    /**
+     * Request for post with header
+     */
+
+
+    public void postWithUrlEncodedRequest(Map<String, String> pHeader, Map<String, String> pParm, String url, ResponseListener pResponseListener) {
+        if (pResponseListener != null) {
+            pResponseListener.onRequestStart();
+        }
+        mUserModel.postWithUrlEncodedRequest(pHeader, pParm, url, pResponseListener);
+    }
+
+
+    /**
+     * Request for GET with header
+     */
+
+
+    public void getRequest(Map<String, String> pHeader, Map<String, String> pParms, String url, ResponseListener pResponseListener) {
+        if (pResponseListener != null) {
+            pResponseListener.onRequestStart();
+        }
+        mUserModel.getRequest(pHeader, pParms, url, pResponseListener);
+    }
+
+
+    /**
+     * Request for Post with json object
+     */
+
+    public void postWithJsonRequest(String url, String token, JSONObject jsonObject, ResponseListener pResponseListener) {
+        if (pResponseListener != null) {
+            pResponseListener.onRequestStart();
+        }
+        mUserModel.postWithJsonRequest(url, token, jsonObject, pResponseListener);
+    }
+
+
+    /**
+     * Request for PUT with header
+     */
+
+
+    public void putRequest(Map<String, String> pHeader, String url, ResponseListener pResponseListener) {
+        if (pResponseListener != null) {
+            pResponseListener.onRequestStart();
+        }
+        mUserModel.putRequest(pHeader, url, pResponseListener);
+    }
+
+
+    /**
+     * Request for Put with json object
+     */
+
+    public void putWithJsonRequest(String url, String token, JSONObject jsonObject, ResponseListener pResponseListener) {
+        if (pResponseListener != null) {
+            pResponseListener.onRequestStart();
+        }
+        mUserModel.putWithJsonRequest(url, token, jsonObject, pResponseListener);
+    }
+
+
+    /**
+     * Request for Post with json Array
+     */
+
+    public void postWithArrayString(String url, String jsonArray, ResponseListener pResponseListener) {
+        if (pResponseListener != null) {
+            pResponseListener.onRequestStart();
+        }
+        mUserModel.postStringRequest(url,  jsonArray, pResponseListener);
+    }
+
+
+}
