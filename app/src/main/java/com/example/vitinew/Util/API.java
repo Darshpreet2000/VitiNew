c867ff0538285a6bb0c4493eb58520df3812f850
Darshpreet2000 <47180477+Darshpreet2000@users.noreply.github.com>
2020-02-03 18:35:29 +0530

    changes

--- app/src/main/java/com/example/vitinew/Util/API.java
+++ app/src/main/java/com/example/vitinew/Util/API.java
@@ -8,4 +8,6 @@ public class API {
     public static final String SIGN_UP = API_DOMAIN + "/api/user/register";
 
     public static final String UPDATE_SKILL = API_DOMAIN + "/api/user/skillsUpdate";
+    public static final String TRUECALLER= API_DOMAIN + "/api/user/loginTC";
+
 }
